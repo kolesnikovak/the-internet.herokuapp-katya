6a27b92d5a253db3fe3957924932381b215ec2b5
kolesnikovak <katyamiller36@gmail.com>
2025-09-22 21:10:16 -0500

    updated code

--- pages/home-page.ts
+++ pages/home-page.ts
@@ -5,10 +5,10 @@ export class HomePage {
     welcomeTitle : Locator
    
     constructor(page: Page) {
-        this.welcomeTitle = page.locator('h1[class="hero-title "] span')
+        
     }
 
     async validateTitleMessage(expectedTitleMessage: string): Promise<void> {
-        await expect(this.welcomeTitle).toHaveText(expectedTitleMessage)
+        
     }
 }
