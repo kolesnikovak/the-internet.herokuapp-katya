65c33ada76a50af8b16cf2aee9682093c666c7a2
kolesnikovak <katyamiller36@gmail.com>
2025-09-24 20:05:52 -0500

    updated code

--- tests/internet.herokuapp.spec.ts
+++ tests/internet.herokuapp.spec.ts
@@ -1,7 +1,7 @@
 import {test} from '@playwright/test'
-import { HomePage } from '../pages/home-page'
-import { StatusCodesPage } from '../pages/status-codes-page'
-import { TyposPage } from '../pages/typos-page'
+import { HomePage } from '../../pages/home-page'
+import { StatusCodesPage } from '../../pages/status-codes-page'
+import { TyposPage } from '../../pages/typos-page'
 
 test('internet herokuapp tests', async ({page}) => {
     let homePage = new HomePage(page)
